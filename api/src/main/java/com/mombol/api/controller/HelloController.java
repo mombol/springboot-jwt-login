86937ba31fe655387157d881f6307394e8b18d7e
mombol <mombol@163.com>
2023-02-24 09:27:35 +0800

    add api module

--- api/src/main/java/com/mombol/api/controller/HelloController.java
+++ api/src/main/java/com/mombol/api/controller/HelloController.java
@@ -1,15 +1,15 @@
-package com.mombol.admin.controller;
+package com.mombol.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping
     public String hello() {
-        return "Hello, Welcome to admin!";
+        return "Hello, Welcome to api!";
     }
-
 }
