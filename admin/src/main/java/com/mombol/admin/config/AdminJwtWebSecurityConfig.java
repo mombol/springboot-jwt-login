46aaea209a9dbb51771b82f24eb53d3c4e82c94f
mombol <mombol@163.com>
2023-03-15 09:39:04 +0800

    rename config file name

---
+++ admin/src/main/java/com/mombol/admin/config/AdminJwtWebSecurityConfig.java
@@ -0,0 +1,8 @@
+package com.mombol.admin.config;
+
+import com.mombol.security.admin.config.JwtWebSecurityConfig;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AdminJwtWebSecurityConfig extends JwtWebSecurityConfig {
+}
--- security/security-admin/src/main/java/com/mombol/security/admin/config/JwtWebSecurityConfig.java
+++ security/security-admin/src/main/java/com/mombol/security/admin/config/JwtWebSecurityConfig.java
@@ -14,8 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration
-public class AdminWebSecurityConfig {
+public class JwtWebSecurityConfig {
 
     @Autowired
     AjaxAuthenticationEntryPoint authenticationEntryPoint;
